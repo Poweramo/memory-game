511fe592a7bf3773e8c1841e48d9f929a8463951
Poweramo <assirouomar@gmail.com>
2024-06-04 19:21:26 +0100

    final version (style remaining)

--- index.js
+++ index.js
@@ -1,100 +1,32 @@
-// Fix: when to cards has matched and two others didn't they don't unflip themselves (flippingCounter problem)
-// TODO: Remove matching-cards class
-
 const cardsContainer = document.querySelector(".game");
 const cardsElements = document.querySelectorAll(".card");
 const scoreElement = document.getElementById("score");
 const failedTriesElement = document.getElementById("failed-attempts");
-let flippingCounter = 0;
 let score = 0;
 let failedTries = 0;
 
-const unflipCards = () => {
-	const flippedCards = document.querySelectorAll(".flipped-card");
-	for (let i = 0; i < flippedCards.length; i++) {
-		setTimeout(() => {
-			flippedCards[i].classList.value = "card";
-
-			flippedCards[i].innerHTML = "";
-			flippingCounter = 0;
-		}, 300);
-		// flippedCards[i].addEventListener("click", () => {
-		// 	flippingCounter = 0;
-		// });
-	}
-};
-
-const didCardsMatched = (img, format) => {
-	const flippedCards = document.querySelectorAll(".flipped-card");
-	if (flippedCards.length > 2) {
-		for (let i = 0; i < flippedCards.length; i++) {
-			flippedCards[i].innerHTML = "";
-			flippedCards[i].classList.remove("flipped-card");
-		}
-	} else {
-		if (flippedCards.length == 2) {
-			if (flippedCards[0].id == flippedCards[1].id) {
-				for (let i = 0; i < flippedCards.length; i++) {
-					flippedCards[
-						i
-					].innerHTML = `<img src="./img/${flippedCards[i].id}.jpg" alt="${flippedCards[i].id}"/>`;
-					flippedCards[i].classList.remove("flipped-card");
-					flippedCards[i].addEventListener("click", () => {
-						flippedCards[
-							i
-						].innerHTML = `<img src="./img/${flippedCards[i].id}.jpg" alt="${flippedCards[i].id}"/>`;
-						flippedCards[i].classList.remove("flipped-card");
-					});
-					flippedCards[i].classList.add("matching-cards");
-				}
-				score++;
-				scoreElement.textContent = `Score : ${score}`;
-			} else {
-				failedTries++;
-				failedTriesElement.textContent = `Failed attempts : ${failedTries}`;
-				for (let i = 0; i < flippedCards.length; i++) {
-					setTimeout(() => {
-						flippedCards[i].innerHTML = "";
-						flippedCards[i].classList.remove("flipped-card");
-					}, 300);
-				}
-			}
-		}
-	}
-};
-
 for (let i = 0; i < cardsContainer.children.length; i++) {
 	const cardElement = cardsContainer.children[i];
-
-	const addCardLogic = (img) => {
-		cardElement.id = img;
-	};
-
 	if (i == 0 || i == 1) {
-		addCardLogic("ninja");
+		cardElement.id = "ninja";
 	} else if (i == 2 || i == 3) {
-		addCardLogic("car");
+		cardElement.id = "car";
 	} else if (i == 4 || i == 5) {
-		addCardLogic("football");
+		cardElement.id = "football";
 	}
 }
 
 for (let i = 0; i < cardsContainer.children.length; i++) {
 	const cardElement = cardsContainer.children[i];
-
-	cardElement.addEventListener("click", () => {
+	cardElement.addEventListener("click", (e) => {
 		cardElement.classList.toggle("flipped-card");
-
 		if (cardElement.hasChildNodes()) {
-			flippingCounter--;
 			cardElement.innerHTML = "";
 		} else {
-			flippingCounter++;
 			switch (cardElement.id) {
 				case "ninja":
-					cardElement.innerHTML = `<img src="./img/ninja.png" alt="ninja"/>`;
+					cardElement.innerHTML = `<img src="./img/ninja.jpg" alt="ninja"/>`;
 					break;
-
 				case "car":
 					cardElement.innerHTML = `<img src="./img/car.jpg" alt="car"/>`;
 					break;
@@ -103,6 +35,44 @@ for (let i = 0; i < cardsContainer.children.length; i++) {
 					break;
 			}
 		}
-		didCardsMatched();
+
+		if (e.target.classList.value !== "") {
+			const flippedCards = document.querySelectorAll(".flipped-card");
+			if (flippedCards.length > 2) {
+				for (let i = 0; i < flippedCards.length; i++) {
+					flippedCards[i].innerHTML = "";
+					flippedCards[i].classList.remove("flipped-card");
+				}
+			} else {
+				if (flippedCards.length == 2) {
+					if (flippedCards[0].id == flippedCards[1].id) {
+						for (let i = 0; i < flippedCards.length; i++) {
+							flippedCards[
+								i
+							].innerHTML = `<img src="./img/${flippedCards[i].id}.jpg" alt="${flippedCards[i].id}"/>`;
+							flippedCards[i].classList.remove("flipped-card");
+							flippedCards[i].addEventListener("click", () => {
+								flippedCards[
+									i
+								].innerHTML = `<img src="./img/${flippedCards[i].id}.jpg" alt="${flippedCards[i].id}"/>`;
+								flippedCards[i].classList.remove("flipped-card");
+							});
+							flippedCards[i].classList.add("matching-cards");
+						}
+						score++;
+						scoreElement.textContent = `Score : ${score}`;
+					} else {
+						failedTries++;
+						failedTriesElement.textContent = `Failed attempts : ${failedTries}`;
+						for (let i = 0; i < flippedCards.length; i++) {
+							setTimeout(() => {
+								flippedCards[i].innerHTML = "";
+								flippedCards[i].classList.remove("flipped-card");
+							}, 300);
+						}
+					}
+				}
+			}
+		}
 	});
 }
